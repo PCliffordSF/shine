07f9fdad7d641534119e10ce9c32887feacf954b
pcliffordsf <paulclifford2570@gmail.com>
2017-08-19 06:30:07 -0700

    add last name search feature

--- app/models/customer_search_term.rb
+++ app/models/customer_search_term.rb
@@ -17,18 +17,20 @@ private
 
   def build_for_name_search(search_term)
     search_array = search_term.split(' ')
+    num_terms = search_array.length
+    search_term = search_array.shift
     @where_clause << case_insensitive_search(:first_name)
-    @where_args[:first_name] = starts_with(search_array[0])
-    if search_array.length == 1
-      @where_clause << " OR #{case_insensitive_search(:last_name)}"
-      @where_args[:last_name] = starts_with(search_array[0])
+    @where_args[:first_name] = starts_with(search_term)
 
-    elsif search_term.split(' ').length == 2
+    if num_terms == 1
+      @where_clause << " OR #{case_insensitive_search(:last_name)}"
+      @where_args[:last_name] = starts_with(search_term)
+    elsif num_terms >= 2
+      last_name = search_array.shift
       @where_clause << " AND #{case_insensitive_search(:last_name)}"
-      @where_args[:last_name] = starts_with(search_array[1])
+      @where_args[:last_name] = starts_with(last_name)
 
     end
-
     @order = "last_name asc"
   end
 
