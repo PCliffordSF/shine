7b290b9b49cf02091f990059d9b9dd9a75a82162
pcliffordsf <paulclifford2570@gmail.com>
2017-08-23 06:35:19 -0700

    Add angular routing

--- app/javascript/packs/customers.js
+++ app/javascript/packs/customers.js
@@ -8,12 +8,25 @@
 ***/
 import "polyfills";
 
-import { NgModule                } from "@angular/core";
-import { BrowserModule           } from "@angular/platform-browser";
-import { FormsModule             } from "@angular/forms";
-import { platformBrowserDynamic  } from "@angular/platform-browser-dynamic";
-import { HttpModule              } from "@angular/http";
-import { CustomerSearchComponent } from "CustomerSearchComponent";
+import { Component, NgModule    } from "@angular/core";
+import { BrowserModule          } from "@angular/platform-browser";
+import { FormsModule            } from "@angular/forms";
+import { platformBrowserDynamic } from "@angular/platform-browser-dynamic";
+import { HttpModule             } from "@angular/http";
+import { RouterModule           } from "@angular/router";
+
+import { CustomerSearchComponent  } from "CustomerSearchComponent";
+import { CustomerDetailsComponent } from "CustomerDetailsComponent";
+
+var AppComponent = Component({
+  selector: "shine-customers-app",
+  template: "<router-outlet></router-outlet>"
+}).Class({
+  constructor: [
+    function() {}
+  ]
+});
+
 
 var RESULTS = [
   {
@@ -53,16 +66,31 @@ var RESULTS = [
   },
 ];
 
-
-
+var routing = RouterModule.forRoot(
+[
+  {
+    path: "",
+    component: CustomerSearchComponent
+  },
+  {
+    path: ":id",
+    component: CustomerDetailsComponent
+  }
+]);
 
 var CustomerAppModule = NgModule({
   imports:      [
     BrowserModule,
     FormsModule,
-    HttpModule ],
-  declarations: [ CustomerSearchComponent ],
-  bootstrap:    [ CustomerSearchComponent ]
+    HttpModule,
+    routing
+  ],
+  declarations: [
+    CustomerSearchComponent,
+    CustomerDetailsComponent,
+    AppComponent
+   ],
+  bootstrap:    [ AppComponent ]
 })
 .Class({
   constructor: function() {}
